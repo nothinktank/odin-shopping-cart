b410f0f68d6d1d6da07db12f5ec8b0625c46c69e
shangwang <wangshangming10@gmail.com>
2025-11-07 16:06:49 -0500

    added react router

---
+++ src/Profile.jsx
@@ -0,0 +1,10 @@
+const Profile = () => {
+    return(
+        <div>
+            <h1>Hello from Profile Page!</h1>
+            <p>how are you?</p>
+        </div>
+    )
+}
+
+export default Profile;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Profile from './Profile.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: 'profile',
+    element: <Profile />
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
