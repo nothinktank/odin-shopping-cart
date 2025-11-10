a224fc5d68b8a488c95ce3bbe51001efd8ab4cd6
shangwang <wangshangming10@gmail.com>
2025-11-10 18:21:43 -0500

    added basic homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
+            <h1>Your Jersey Shop</h1>
+            <div className="image">
+            <img src="https://shoplineimg.com/5f680caef39524001ca7be41/6911bd4ff869c600183cd28a/800x.webp?source_format=JPG" alt="" />
+
+            </div>
         </div>
     )
 }
