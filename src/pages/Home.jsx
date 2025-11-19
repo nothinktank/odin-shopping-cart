d4706b107ae6f9a20d1f796723015c13106f4c5f
shangwang <wangshangming10@gmail.com>
2025-11-19 17:54:56 -0500

    import the image instead of referencing it in the component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
+import React from "react";
+import jardinImage from "../assets/cover.png";
+
 const Home = () => {
     return (
         <div>
             <h1>Le Jardin</h1>
             <div>
-            <img className="home-image" src="./src/assets/cover.png" alt="" />
+            <img className="home-image" src={jardinImage} alt="" />
 
             </div>
         </div>
